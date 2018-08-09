d9282d9448cc463e621939436210d5ead45da1fe
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2018-08-09 15:01:32 -0500

    Create pdf2txt.py

---
+++ pdf2txt.py
@@ -0,0 +1,5 @@
+pdf2txt.py -o output.html -t filename.pdf
+
+This will convert the file to an html. may not be pretty. maybe xml is better? Haven't tried to read this html with Beautifulsoup yet.
+
+Has to have the file name in the base directory of the anaconda prompt.
